2d3b3d12e050c5096a54a9f906c54e8e9fc9c870
kapil.foss@gmail.com <>
2010-05-04 10:25:25 -0400

    supress client zookeeper logs again for unit tests

--- txzookeeper/tests/__init__.py
+++ txzookeeper/tests/__init__.py
@@ -4,10 +4,9 @@ import sys
 from twisted.trial.unittest import TestCase
 from mocker import MockerTestCase
 
-from txzookeeper.client import Wrapper
+#from txzookeeper.client import Wrapper
 
-zookeeper = Wrapper(zookeeper)
-
+#zookeeper = Wrapper(zookeeper)
 
 class ZookeeperTestCase(TestCase, MockerTestCase):
 
@@ -16,7 +15,7 @@ class ZookeeperTestCase(TestCase, MockerTestCase):
         self.log_file_path = self.makeFile()
         self.log_file = open(self.log_file_path, 'w')
         zookeeper.set_debug_level(zookeeper.LOG_LEVEL_DEBUG)
-        #zookeeper.set_log_stream(self.log_file)
+        zookeeper.set_log_stream(self.log_file)
 
     def tearDown(self):
         self.log_file.close()
